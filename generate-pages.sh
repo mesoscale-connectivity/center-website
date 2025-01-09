323e3f900b1d96995c65d127718284dfcd3506af
Taylor Grafft <taylorgrafft@bettle.psy.utexas.edu>
2025-01-09 13:24:10 -0600

    renaming output to docs

--- generate-pages.sh
+++ generate-pages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Create output directory
-mkdir -p output
+# Create docs directory
+mkdir -p docs
 
 # Generate each page
-xsltproc --output output/index.html index.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/working-groups.html working-groups.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/institutions.html institutions.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/publications.html publications.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/jobs.html jobs.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/data.html data.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/tools.html tools.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/contact.html contact.xsl Squarespace-Wordpress-Export-12-12-2024.xml
-xsltproc --output output/press.html press.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/index.html index.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/working-groups.html working-groups.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/institutions.html institutions.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/publications.html publications.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/jobs.html jobs.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/data.html data.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/tools.html tools.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/contact.html contact.xsl Squarespace-Wordpress-Export-12-12-2024.xml
+xsltproc --output docs/press.html press.xsl Squarespace-Wordpress-Export-12-12-2024.xml
